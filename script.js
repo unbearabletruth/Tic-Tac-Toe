9f4a53f7b09f97275b47b0fb0bb2c1c115c70b44
unbearabletruth <antaarty@gmail.com>
2023-04-10 11:24:50 +0700

    Logic and interface separated

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const squares = document.querySelectorAll(".square");
+const winnertext = document.querySelector("#title");
 
 const Players = (name, weapon) => {
     return {name, weapon};
@@ -9,20 +10,7 @@ const Player2 = Players("Darwin", "o");
 
 const gameBoardModule = (() => {    
     gameBoard = ["", "", "", "", "", "", "", "", ""];
-
-    return {gameBoard};
-})();
-
-const displayController = (() => {
-    const {gameBoard} = gameBoardModule;
     let player = Player1
-    
-    const render = () => {
-    for (let i = 0; i < gameBoard.length; i++) {
-        const current = document.getElementById(`${i}`);
-        current.textContent = gameBoard[i];
-     }
-    };
 
     const fillArray = (square) => {
         if (square.textContent != "x" && square.textContent != "o"){
@@ -34,24 +22,46 @@ const displayController = (() => {
                 gameBoard.splice(square.id, 1, player.weapon);
                 player = Player1
             }
-            render();
+        }  
+    };
+
+    const checkWin = () => {
+        if (gameBoard[0] === gameBoard[1] && gameBoard[0] === gameBoard[2] && gameBoard[0] != ""){
+            console.log(gameBoard[0])
+            if (gameBoard[0] === `${Player1.weapon}`){
+                winnertext.textContent = `we have a winner: ${Player1.name}`;
+        }else {
+            winnertext.textContent = `we have a winner: ${Player2.name}`
         }
-        
+    }
+    };
+
+    return {gameBoard, fillArray, checkWin};
+})();
+
+
+const displayController = (() => {
+    const board = Object.create(gameBoardModule);
+
+    const render = () => {
+    for (let i = 0; i < gameBoard.length; i++) {
+        const current = document.getElementById(`${i}`);
+        current.textContent = gameBoard[i];
+     }
     };
 
     const fillBoard = () => {
         squares.forEach(square => {
             square.addEventListener("click", () => {
-                fillArray(square);
+                board.fillArray(square);
+                board.checkWin();
+                render();
             })
         });
         };
     
-    
     fillBoard();
-
-    
-    return {render};
+    return {render, fillBoard};
 })();
 
 
